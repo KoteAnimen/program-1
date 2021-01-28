1f798a79811725f8417b51335cbe44265581b1ad
Kote\Kote_Animen <sverh.il@yandex.ru>
2021-01-28 20:20:59 +0300

    Добавьте файлы проекта.

---
+++ program 1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace program_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.listNumbers = new System.Windows.Forms.ListBox();
+            this.sizeMass = new System.Windows.Forms.NumericUpDown();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.GetAnswer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minNumber = new System.Windows.Forms.NumericUpDown();
+            this.maxNumber = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeMass)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.table);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 160);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Результат";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.listNumbers);
+            this.groupBox2.Location = new System.Drawing.Point(13, 179);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(459, 259);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Массив значений";
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(7, 20);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(443, 134);
+            this.table.TabIndex = 0;
+            // 
+            // listNumbers
+            // 
+            this.listNumbers.FormattingEnabled = true;
+            this.listNumbers.Location = new System.Drawing.Point(7, 20);
+            this.listNumbers.Name = "listNumbers";
+            this.listNumbers.Size = new System.Drawing.Size(200, 225);
+            this.listNumbers.TabIndex = 0;
+            // 
+            // sizeMass
+            // 
+            this.sizeMass.Location = new System.Drawing.Point(6, 19);
+            this.sizeMass.Name = "sizeMass";
+            this.sizeMass.Size = new System.Drawing.Size(225, 20);
+            this.sizeMass.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sizeMass);
+            this.groupBox3.Location = new System.Drawing.Point(213, 20);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(237, 53);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Размер массива";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.maxNumber);
+            this.groupBox4.Controls.Add(this.GetAnswer);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.minNumber);
+            this.groupBox4.Location = new System.Drawing.Point(213, 79);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(237, 166);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Диапазон значений массива";
+            // 
+            // GetAnswer
+            // 
+            this.GetAnswer.Location = new System.Drawing.Point(6, 127);
+            this.GetAnswer.Name = "GetAnswer";
+            this.GetAnswer.Size = new System.Drawing.Size(225, 33);
+            this.GetAnswer.TabIndex = 3;
+            this.GetAnswer.Text = "Получить ответ";
+            this.GetAnswer.UseVisualStyleBackColor = true;
+            this.GetAnswer.Click += new System.EventHandler(this.GetAnswer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Минимальное число";
+            // 
+            // minNumber
+            // 
+            this.minNumber.Location = new System.Drawing.Point(6, 36);
+            this.minNumber.Name = "minNumber";
+            this.minNumber.Size = new System.Drawing.Size(225, 20);
+            this.minNumber.TabIndex = 1;
+            // 
+            // maxNumber
+            // 
+            this.maxNumber.Location = new System.Drawing.Point(6, 88);
+            this.maxNumber.Name = "maxNumber";
+            this.maxNumber.Size = new System.Drawing.Size(225, 20);
+            this.maxNumber.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Максимальное число";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вариант 8 Филяк";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeMass)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxNumber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown maxNumber;
+        private System.Windows.Forms.Button GetAnswer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown minNumber;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.NumericUpDown sizeMass;
+        private System.Windows.Forms.ListBox listNumbers;
+    }
+}
+
---
+++ program 1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program_1
+{
+    public partial class Form1 : Form
+    {
+        public int[] mass;
+        public int countNumbers;
+        public int currentNumber;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetAnswer_Click(object sender, EventArgs e)
+        {
+            mass = new int[(int)sizeMass.Value];
+            currentNumber = (int)minNumber.Value;
+            table.RowCount = 2;
+
+            for(int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next((int)minNumber.Value, (int)maxNumber.Value);
+                listNumbers.Items.Add(mass[i]);
+            }
+            for (int i = 0; i < mass.Length; i++)
+            {
+                for(int j = 0; j < mass.Length; j++)
+                {
+                    if(mass[j] == currentNumber)
+                    {
+                        countNumbers++;
+                    }
+                }
+                if(countNumbers != 0)
+                {
+                    table.ColumnCount += 1;
+                    table[table.ColumnCount - 1, 0].Value = currentNumber;
+                    table[table.ColumnCount - 1, 1].Value = countNumbers;
+                }
+                countNumbers = 0;
+                currentNumber++;
+            }
+        }
+    }
+}
